f184d788a42c43d31f6c413bb5315e3a36dd2d4e
Sanjay Cyberpoint <varsha.cyberpointmedia@gmail.com>
2024-06-04 12:02:23 +0530

    intil commit

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,18 @@
 
 #include "generated_plugin_registrant.h"
 
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+}
+
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
 #include <file_selector_linux/file_selector_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,17 @@
 import FlutterMacOS
 import Foundation
 
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+}
+
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
 import connectivity_plus
 import file_selector_macos
 import flutter_image_compress_macos
