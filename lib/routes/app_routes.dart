2572d3b4efc4bac89264496f686c3bc891f74bf6
Ajit Singh <rathoreajit991@gmail.com>
2024-07-16 12:52:57 +0530

    bugs resolving 3

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -9,14 +9,8 @@ import 'package:experta/presentation/add_bank%20_account/add_bank_account.dart';
 import 'package:experta/presentation/add_bank%20_account/binding/add_bank_account_binding.dart';
 import 'package:experta/presentation/add_upi/add_upi.dart';
 import 'package:experta/presentation/add_upi/binding/add_upi_binding.dart';
-import 'package:experta/presentation/additional_info/additional_info_page.dart';
-import 'package:experta/presentation/additional_info/binding/additional_binding.dart';
-import 'package:experta/presentation/additional_info/binding/edit_interest_binding.dart';
-import 'package:experta/presentation/additional_info/edit_interest.dart';
 import 'package:experta/presentation/blocked/binding/blocked_binding.dart';
 import 'package:experta/presentation/blocked/blocked.dart';
-import 'package:experta/presentation/call_settings/bindings/call_setting_bindings.dart';
-import 'package:experta/presentation/call_settings/call_settings.dart';
 import 'package:experta/presentation/category/category_controller.dart';
 import 'package:experta/presentation/category/category_screen.dart';
 import 'package:experta/presentation/change_date_of_birth/binding/change_date_of_birth_binding.dart';
@@ -26,19 +20,14 @@ import 'package:experta/presentation/change_email/change_email.dart';
 import 'package:experta/presentation/change_gender/binding/change_gender_binding.dart';
 import 'package:experta/presentation/change_gender/change_gender.dart';
 import 'package:experta/presentation/change_user_name/binding/change_user_name_binding.dart';
-import 'package:experta/presentation/change_user_name/change_user_name.dart';
 import 'package:experta/presentation/createPost/binding/create_post_binding.dart';
 import 'package:experta/presentation/createPost/create_post.dart';
 import 'package:experta/presentation/dashboard/binding/dashboard_binding.dart';
 import 'package:experta/presentation/dashboard/dashboard.dart';
-import 'package:experta/presentation/edit_education/binding/edit_education_list_binding.dart';
-import 'package:experta/presentation/edit_education/edit_education.dart';
 import 'package:experta/presentation/edit_profile/edit_profile_binding/edit_profile_binding.dart';
 import 'package:experta/presentation/edit_profile/edit_profile_setting.dart';
 import 'package:experta/presentation/edit_work_experience/binding/edit_work_experience_binding.dart';
 import 'package:experta/presentation/edit_work_experience/edit_work_experience.dart';
-import 'package:experta/presentation/education/binding/education_list_binding.dart';
-import 'package:experta/presentation/education/education_edit.dart';
 import 'package:experta/presentation/feeds_active_screen/binding/feeds_active_binding.dart';
 import 'package:experta/presentation/feeds_active_screen/feeds_active_screen.dart';
 import 'package:experta/presentation/followers/binding/followers_binding.dart';
@@ -70,19 +59,12 @@ import 'package:experta/presentation/search_screen/binding/search_binding.dart';
 import 'package:experta/presentation/search_screen/search_screen.dart';
 import 'package:experta/presentation/security_privacy/binding/security_privacy_binding.dart';
 import 'package:experta/presentation/security_privacy/security_privacy.dart';
-import 'package:experta/presentation/set_availability/bindings/set_availability_bindings.dart';
-import 'package:experta/presentation/set_availability/edit_set_avail/bindings/edit_set_avail_bindings.dart';
-import 'package:experta/presentation/set_availability/edit_set_avail/edit_set_avail.dart';
-import 'package:experta/presentation/set_availability/set_availability.dart';
-import 'package:experta/presentation/set_pricing/bindings/set_pricing_bindings.dart';
-import 'package:experta/presentation/set_pricing/set_pricing.dart';
 import 'package:experta/presentation/setting_screen/binding/setting_binding.dart';
 import 'package:experta/presentation/setting_screen/setting_screen.dart';
 import 'package:experta/presentation/signin_page/signin_binding/signin_binding.dart';
 import 'package:experta/presentation/signin_page/signin_page.dart';
 import 'package:experta/presentation/splash_screen/binding/splash_binding.dart';
 import 'package:experta/presentation/splash_screen/splash_screen.dart';
-import 'package:experta/presentation/verify_account/binding/verify_account_binding.dart';
 import 'package:experta/presentation/verify_account/varify_account.dart';
 import 'package:experta/presentation/verifynumber_screen/binding/verifynumber_binding.dart';
 import 'package:experta/presentation/verifynumber_screen/verifynumber_screen.dart';
@@ -181,8 +163,6 @@ class AppRoutes {
   static const String changegender = "/change_gender";
 
   static const String accountSetting = "/account_setting";
-
-  static const String editExperience = '/edit_work_experience';
 
   static const String aboutus = "/about_us";
 
@@ -304,13 +284,13 @@ class AppRoutes {
         AccountSettingBinding(),
       ],
     ),
-    GetPage(
-      name: bank,
-      page: () => const VerifyAccount(),
-      bindings: [
-        VerifyAccountBinding(),
-      ],
-    ),
+    // GetPage(
+    //   name: bank,
+    //   page: () => const VerifyAccount(),
+    //   bindings: [
+    //     VerifyAccountBinding(),
+    //   ],
+    // ),
     GetPage(
       name: verifynumberScreen,
       page: () => const VerifynumberScreen(),
@@ -332,13 +312,13 @@ class AppRoutes {
         SearchBinding(),
       ],
     ),
-    GetPage(
-        name: editExperience,
-        page: () => const EditWorkExperiencePage(),
-        customTransition: CustomPageTransition(),
-        bindings: [
-          EditWorkExperienceBinding(),
-        ]),
+    // GetPage(
+    //     name: editExperience,
+    //     page: () => const EditWorkExperiencePage(),
+    //     customTransition: CustomPageTransition(),
+    //     bindings: [
+    //       EditWorkExperienceBinding(),
+    //     ]),
     GetPage(
       name: wallet,
       page: () => const Wallet(),
@@ -381,13 +361,13 @@ class AppRoutes {
         WalletBinding(),
       ],
     ),
-    GetPage(
-      name: changeUserName,
-      page: () => const ChangeUserName(),
-      bindings: [
-        ChangeUserNameBinding(),
-      ],
-    ),
+    // GetPage(
+    //   name: changeUserName,
+    //   page: () => const ChangeUserName(),
+    //   bindings: [
+    //     ChangeUserNameBinding(),
+    //   ],
+    // ),
     GetPage(
         name: notification,
         page: () => const NotificationScreen(),
@@ -426,75 +406,6 @@ class AppRoutes {
         page: () => const EditProfessionalInfo(),
         bindings: [
           EditProfessionalInfoBinding(),
-        ]),
-    GetPage(
-        name: experience,
-        page: () => const WorkExperiencePage(),
-        bindings: [
-          WorkExperienceBinding(),
-        ]),
-    GetPage(
-        name: editExperience,
-        page: () => const EditWorkExperiencePage(),
-        customTransition: CustomPageTransition(),
-        bindings: [
-          EditWorkExperienceBinding(),
-        ]),
-    GetPage(
-        name: education,
-        page: () => const EducationList(),
-        customTransition: CustomPageTransition(),
-        bindings: [
-          EducationBinding(),
-        ]),
-    GetPage(
-        name: editEducation,
-        page: () => const EditEducationPage(),
-        customTransition: CustomPageTransition(),
-        bindings: [
-          EditEducationBinding(),
-        ]),
-    GetPage(
-        name: additional,
-        page: () => const AdditionalInfoPage(),
-        customTransition: CustomPageTransition(),
-        bindings: [
-          AdditionalInfoBinding(),
-        ]),
-    GetPage(
-        name: editInterest,
-        page: () => const EditInterestPage(),
-        customTransition: CustomPageTransition(),
-        bindings: [
-          EditInterestBinding(),
-        ]),
-    GetPage(
-        name: callSettings,
-        page: () => const CallSettings(),
-        customTransition: CustomPageTransition(),
-        bindings: [
-          CallSettingsBindings(),
-        ]),
-    GetPage(
-        name: setAvailability,
-        page: () => const SetAvailability(),
-        customTransition: CustomPageTransition(),
-        bindings: [
-          SetAvailabilityBindings(),
-        ]),
-    GetPage(
-        name: editSetAvail,
-        page: () => const EditSetAvailability(),
-        customTransition: CustomPageTransition(),
-        bindings: [
-          EditSetAvailableBinding(),
-        ]),
-    GetPage(
-        name: setPricing,
-        page: () => const SetPricing(),
-        customTransition: CustomPageTransition(),
-        bindings: [
-          SetPricingBindings(),
         ]),
     GetPage(name: category, page: () => CategoryScreen(), bindings: [
       CategoryBinding(),
