7984b62e56966d733eee148fb7acabd2c11ab390
rathoreAjitSingh-1887 <ajit@cyberpointmedia.com>
2024-07-17 15:38:08 +0530

    glide module

--- android/app/src/main/AppGlide?Module.java
+++
@@ -1,8 +0,0 @@
-// MyAppGlideModule.java
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class MyAppGlideModule extends AppGlideModule {
-    // Leave empty for now
-}
