8191ed3aba983db62bc7c73d8b9cbdf74eae81b3
rathoreAjitSingh-1887 <ajit@cyberpointmedia.com>
2024-10-04 15:18:07 +0530

    resolved rebasing problem

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -7,7 +7,9 @@
 // ignore_for_file: type=lint
 
 import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter_web.dart';
 import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:flutter_image_compress_web/flutter_image_compress_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
 import 'package:image_cropper_for_web/image_cropper_for_web.dart';
@@ -20,7 +22,9 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   ConnectivityPlusWebPlugin.registerWith(registrar);
+  EmojiPickerFlutterPluginWeb.registerWith(registrar);
   FilePickerWeb.registerWith(registrar);
+  FlutterImageCompressWeb.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
   GeolocatorPlugin.registerWith(registrar);
   ImageCropperPlugin.registerWith(registrar);
