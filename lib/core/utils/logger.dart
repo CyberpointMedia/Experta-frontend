5acd33f0c0ec3d72d608d8c4222a4b72d6eb3e1d
rathoreAjitSingh-1887 <ajit@cyberpointmedia.com>
2024-11-19 13:22:50 +0530

    apiService class logger added

--- lib/core/utils/logger.dart
+++ lib/core/utils/logger.dart
@@ -1,3 +1,5 @@
+enum LogMode { debug, live }
+
 class Logger {
   static LogMode _logMode = LogMode.debug;
 
@@ -5,11 +7,39 @@ class Logger {
     Logger._logMode = mode;
   }
 
-  static void log(dynamic data, {StackTrace? stackTrace}) {
+  static void error(dynamic data, {StackTrace? stackTrace}) {
     if (_logMode == LogMode.debug) {
-      print("Error: $data$stackTrace");
+      print("ERROR: $data${stackTrace != null ? '\n$stackTrace' : ''}");
+    }
+  }
+
+  static void info(dynamic data) {
+    if (_logMode == LogMode.debug) {
+      print("INFO: $data");
+    }
+  }
+
+  static void debug(dynamic data) {
+    if (_logMode == LogMode.debug) {
+      print("DEBUG: $data");
+    }
+  }
+
+  static void request(String method, String url,
+      {dynamic body, dynamic headers}) {
+    if (_logMode == LogMode.debug) {
+      print("REQUEST[$method] $url");
+      if (headers != null) print("Headers: $headers");
+      if (body != null) print("Body: $body");
+    }
+  }
+
+  static void response(
+      String method, String url, int statusCode, dynamic body) {
+    if (_logMode == LogMode.debug) {
+      print("RESPONSE[$method] $url");
+      print("Status Code: $statusCode");
+      print("Body: $body");
     }
   }
 }
-
-enum LogMode { debug, live }
