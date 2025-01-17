ca46dccd9be34eb682abf095f4dfab411f7054db
rathoreAjitSingh-1887 <ajit@cyberpointmedia.com>
2025-01-17 15:45:03 +0530

    rebasing

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/3.19.1/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/apple/Desktop/development/experta/experta_frontend"
+export "FLUTTER_ROOT=C:\source\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\source\experta\Experta-frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
